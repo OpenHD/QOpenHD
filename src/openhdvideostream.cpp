583f274664d35fe448893a7409a869e1bcfb80aa
Stephen Oliver <steve@infincia.com>
2019-10-31 01:05:43 -0400

    Remove all conditional compile checks in vidstream

--- src/openhdvideostream.cpp
+++ src/openhdvideostream.cpp
@@ -19,39 +19,29 @@ void OpenHDVideoStream::init() {
 }
 
 void OpenHDVideoStream::_start() {
-#if defined(ENABLE_VIDEO)
     qDebug() << "OpenHDVideoStream::_start()";
     if (!m_receiver.running()) {
         m_receiver.start();
     }
-#endif
 }
 
 void OpenHDVideoStream::startVideo() {
-#if defined(ENABLE_VIDEO)
     QFuture<void> future = QtConcurrent::run(this, &OpenHDVideoStream::_start);
-#endif
 }
 
 void OpenHDVideoStream::_stop() {
-#if defined(ENABLE_VIDEO)
     qDebug() << "OpenHDVideoStream::_stop()";
     if (m_receiver.running()) {
         m_receiver.stop();
     }
-#endif
 }
 
 void OpenHDVideoStream::stopVideo() {
-#if defined(ENABLE_VIDEO)
     QFuture<void> future = QtConcurrent::run(this, &OpenHDVideoStream::_stop);
-#endif
 }
 
 void OpenHDVideoStream::setUri(QString uri) {
     m_uri = uri;
     emit uriChanged(m_uri);
-    #if defined(ENABLE_VIDEO)
     m_receiver.setUri(m_uri);
-    #endif
 }
