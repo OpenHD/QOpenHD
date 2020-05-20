1bb671a129f60082cbb5a88817977df04286e442
Stephen Oliver <steve@infincia.com>
2020-05-20 00:36:30 -0400

    Add level 1 migration for bitrate/downlink rssi

--- src/migration.cpp
+++ src/migration.cpp
@@ -10,6 +10,7 @@
  *
  * Changes requiring migration will be documented here.
  *
+ * 1: reset the bitrate and downlink rssi widget sizes since they became larger and changed location
  *
  *
  */
@@ -30,6 +31,21 @@ void Migration::run() {
 
     auto migration_level = settings.value("migration_level", 0).toInt();
 
+    if (migration_level < 1) {
+        settings.remove("bitrate_widget_x_offset");
+        settings.remove("bitrate_widget_y_offset");
+        settings.remove("bitrate_widget_h_center");
+        settings.remove("bitrate_widget_v_center");
+        settings.remove("bitrate_widget_align");
+
+        settings.remove("downlink_rssi_widget_x_offset");
+        settings.remove("downlink_rssi_widget_y_offset");
+        settings.remove("downlink_rssi_widget_h_center");
+        settings.remove("downlink_rssi_widget_v_center");
+        settings.remove("downlink_rssi_widget_align");
+
+        settings.setValue("migration_level", 1);
+    }
 
     emit migrationsFinished();
 }
