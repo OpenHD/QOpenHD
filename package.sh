c5ece0b8086d7bbc8fbc4dbd822c5ff3ffac6811
Stephen Oliver <steve@infincia.com>
2020-07-07 00:04:39 -0400

    Fix location of QOpenHD binary in package script

--- package.sh
+++ package.sh
@@ -36,7 +36,7 @@ mkdir -p ${TMPDIR}/usr/local/share/openhd || exit 1
 make clean || exit 1
 
 make -j4 || exit 1
-cp QOpenHD ${TMPDIR}/usr/local/bin/ || exit 1
+cp release/QOpenHD ${TMPDIR}/usr/local/bin/ || exit 1
 
 # included in the same package since it's sharing code and not independently versioned
 pushd OpenHDBoot
