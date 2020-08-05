043940eb62369a22d56cdd1b75f999f2a383fb39
pilotnbr1 <pilotnbr1@gmail.com>
2020-08-05 15:42:00 +0200

    Speedladder fix 3 digit text alignment

--- src/speedladder.cpp
+++ src/speedladder.cpp
@@ -49,7 +49,12 @@ void SpeedLadder::paint(QPainter* painter) {
                 painter->fillRect(QRectF(x, y, 12, 3), m_color);
 
                 if (k > speed + 5 || k < speed - 5) {
-                    painter->drawText(x_label, y + 6, QString::number(k));
+                    if (QString::number(k).count()>2){ //workaround cuz qfont does not have align
+                        painter->drawText(x_label-10, y + 6, QString::number(k));
+                    }
+                    else {
+                        painter->drawText(x_label, y + 6, QString::number(k));
+                    }
                 }
             }
             if (k < m_speedMinimum) {
