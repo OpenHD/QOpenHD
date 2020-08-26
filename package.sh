fc72c8b6c7dfee9fb914bc8c03496c1e074683aa
Stephen Oliver <steve@infincia.com>
2020-08-26 00:20:37 -0400

    Push packages to 2.1 repo as well as 2.0
    The packages are identical in each repo, just like the qt package.

--- package.sh
+++ package.sh
@@ -76,7 +76,9 @@ git describe --exact-match HEAD > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
     echo "Pushing package to OpenHD repository"
     cloudsmith push deb openhd/openhd-2-0/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
+    cloudsmith push deb openhd/openhd-2-1/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
 else
     echo "Pushing package to OpenHD testing repository"
     cloudsmith push deb openhd/openhd-2-0-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
+    cloudsmith push deb openhd/openhd-2-1-testing/${OS}/${DISTRO} ${PACKAGE_NAME}_${VERSION//v}_${PACKAGE_ARCH}.deb
 fi
