9697c3a9fa51f8780b62382a0094be32188686c1
Stephen Oliver <steve@infincia.com>
2021-03-18 12:12:01 -0400

    Fix missing bash syntax

--- package.sh
+++ package.sh
@@ -84,7 +84,7 @@ fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION//v} -C ${TM
   -d "gstreamer1.0-libav" \
   -d "gstreamer1.0-tools" \
   -d "gstreamer1.0-alsa" \
-  -d "gstreamer1.0-pulseaudio"
+  -d "gstreamer1.0-pulseaudio" \
   -d "openhd-qt >= 5.15.0" || exit 1
 
 #
