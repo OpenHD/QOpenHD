fec8270452fbe36b8a3e7d581ce4089de6c94e5b
Luke <pilotnbr1@gmail.com>
2021-06-23 20:38:18 -0400

    Removed "make clean" statement (was causing errors)

--- package.sh
+++ package.sh
@@ -61,7 +61,7 @@ cp release/QOpenHD ${TMPDIR}/usr/local/bin/ || exit 1
 # included in the same package since it's sharing code and not independently versioned
 pushd OpenHDBoot
 /opt/${QT_VERSION}/bin/qmake
-make clean || exit 1
+#make clean || exit 1
 make -j4 || exit 1
 cp OpenHDBoot ${TMPDIR}/usr/local/bin/ || exit 1
 popd
