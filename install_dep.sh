519bf394b69d148fa8d2fefa0070ab8a2c65b90c
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-06-22 21:12:27 +0200

    build plugins good also on x86
    .. fmt is not (yet) availiable for x86, so installing fails, but if I change the order it'll install meson, which is needed for building gstreamer

--- install_dep.sh
+++ install_dep.sh
@@ -4,6 +4,6 @@
 
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good \
     ruby ruby-dev rubygems
-apt -y install git fmt meson
+apt -y install git meson fmt
 
 gem install fpm
