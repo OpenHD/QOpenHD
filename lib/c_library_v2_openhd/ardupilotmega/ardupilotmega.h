8e0a4be9189945e36512f94cf7a493b0cf1b83e9
consti10 <geierconstantinabc@gmail.com>
2022-07-12 01:03:36 +0200

    update mavlink

--- lib/c_library_v2_openhd/ardupilotmega/ardupilotmega.h
+++ lib/c_library_v2_openhd/ardupilotmega/ardupilotmega.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ARDUPILOTMEGA.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ARDUPILOTMEGA_XML_HASH 10874096039900579
+#define MAVLINK_ARDUPILOTMEGA_XML_HASH 9031430357524231337
 
 #ifdef __cplusplus
 extern "C" {
