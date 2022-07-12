c46c7979e78803250c7c6e71433b2bfb5bdda2fe
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-12 21:45:23 +0200

    add mavsdk package to build environment

--- install_dep.sh
+++ install_dep.sh
@@ -2,7 +2,7 @@
 
 # Install all the dependencies needed to build QOpenHD from source.
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good \
-    ruby ruby-dev rubygems
+    ruby ruby-dev rubygems mavsdk
 apt -y install git meson fmt
 
 gem install fpm
