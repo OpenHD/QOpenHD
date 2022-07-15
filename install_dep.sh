80554409efbc338ee8a7347478fc997f13cceedd
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-07-15 03:03:40 +0200

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# Install all the dependencies needed to build QOpenHD from source.
-apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good \
-    ruby ruby-dev rubygems mavsdk
+# Install all the dependencies needed to build QOpenHD from source
+apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
+apt -y install ruby ruby-dev 
+apt -y install rubygems libgemplugin-ruby
+apt -y install mavsdk
 apt -y install git meson fmt
 
 gem install fpm
