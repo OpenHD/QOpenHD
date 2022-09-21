aab6fa7b4137ace1e3317fbfa9337bf65e4ede05
consti10 <geierconstantinabc@gmail.com>
2022-09-21 23:06:55 +0200

    exp

--- app/exp/mmal/graph_decode_render.h
+++ app/exp/mmal/graph_decode_render.h
@@ -1,13 +1,13 @@
+
+extern "C"{
 #include "bcm_host.h"
-#include <interface/mmal/mmal.h>
-#include <interface/mmal/util/mmal_connection.h>
-#include <interface/mmal/util/mmal_graph.h>
-#include <interface/mmal/util/mmal_util.h>
-#include <interface/mmal/util/mmal_default_components.h>
-#include <interface/mmal/util/mmal_util_params.h>
+#include "mmal.h"
+#include "util/mmal_graph.h"
+#include "util/mmal_default_components.h"
+#include "util/mmal_util_params.h"
 #include <stdio.h>
 #include "interface/vcos/vcos.h"
-
+}
 
 
 #define CHECK_STATUS(status, msg) if (status != MMAL_SUCCESS) { fprintf(stderr, msg"\n"); goto error; }
