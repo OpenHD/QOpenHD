d899ae1b6a1529c0ff8d7c00fcf6f8b8417ff239
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-24 02:21:24 +0200

    update dependencies

--- install_dep_for_openhd.sh
+++ install_dep_for_openhd.sh
@@ -1,5 +1,5 @@
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good \
-    ruby ruby-dev rubygems
+    ruby ruby-dev rubygems libavcodec-dev
 apt -y install git meson fmt
 
 gem install fpm
