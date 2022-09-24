ff1509e5c55f417e8579d1de9d3ddcf6a27c3ada
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-24 02:31:29 +0200

    add format

--- install_dep.sh
+++ install_dep.sh
@@ -2,7 +2,7 @@
 
 # Install all the dependencies needed to build QOpenHD from source
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
-apt -y install ruby ruby-dev libavcodec-dev
+apt -y install ruby ruby-dev libavcodec-dev libavformat-dev
 apt -y install rubygems libgemplugin-ruby
 apt -y install mavsdk
 apt -y install git meson fmt
