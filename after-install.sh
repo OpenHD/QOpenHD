9261e8c24fb80914592fa66388060d93180f898a
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-25 15:17:45 +0200

    change path

--- after-install.sh
+++ after-install.sh
@@ -3,9 +3,5 @@
 # the openhd scripts start the correct OSD depending on the distro and settings
 systemctl enable qopenhd.service
 
-mkdir -p /usr/local/share/testvideos
-cd /usr/local/share/testvideos
-git clone https://github.com/OpenHD/TestVideos/
-cd TestVideos mv * ../
-cd /usr/local/share/testvideos
-rm -Rf TestVideos
+cd /usr/local/share/
+git clone https://github.com/OpenHD/testvideos/
