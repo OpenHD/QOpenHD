41fc41b3b31f99e77eafd40d76c61ffc7cfe0bcf
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-27 00:22:13 +0200

    if testvideos are there do not clone

--- after-install.sh
+++ after-install.sh
@@ -4,4 +4,9 @@
 systemctl enable qopenhd.service
 
 cd /usr/local/share/
+if [ -d "/usr/local/share/testvideos" ]; then
+  # Take action if $DIR exists. #
+  echo "Testvideos are already there"
+else
 git clone https://github.com/OpenHD/testvideos/
+fi
