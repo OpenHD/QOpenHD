8641a63117ab4785e55954a94d6d6c4aefa274fc
consti10 <geierconstantinabc@gmail.com>
2022-09-29 15:55:18 +0200

    mmal

--- app/exp/mmal/rpimmaldecoder.cpp
+++ app/exp/mmal/rpimmaldecoder.cpp
@@ -1,6 +1,6 @@
 #include "rpimmaldecoder.h"
 
-rpimmaldecoder::rpimmaldecoder()
+RPIMMALDecoder::RPIMMALDecoder()
 {
 
 }
--- app/exp/mmal/rpimmaldecoder.h
+++ app/exp/mmal/rpimmaldecoder.h
@@ -1,11 +1,22 @@
 #ifndef RPIMMALDECODER_H
 #define RPIMMALDECODER_H
 
+#include "bcm_host.h"
+#include "interface/mmal/mmal.h"
+#include "interface/mmal/util/mmal_default_components.h"
+#include "interface/vcos/vcos.h"
 
-class rpimmaldecoder
+struct CONTEXT_T {
+    VCOS_SEMAPHORE_T in_semaphore;
+    VCOS_SEMAPHORE_T out_semaphore;
+    MMAL_QUEUE_T *queue;
+};
+
+
+class RPIMMALDecoder
 {
 public:
-    rpimmaldecoder();
+    RPIMMALDecoder();
 };
 
 #endif // RPIMMALDECODER_H
