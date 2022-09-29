11d1b3fa2c8355258737e7846d640ef1cd122ba3
consti10 <geierconstantinabc@gmail.com>
2022-09-29 20:08:00 +0200

    exp mmal

--- app/exp/mmal/rpimmaldecodedisplay.h
+++ app/exp/mmal/rpimmaldecodedisplay.h
@@ -1,13 +1,13 @@
 #ifndef RPIMMALDECODEDISPLAY_H
 #define RPIMMALDECODEDISPLAY_H
 
-#include "bcm_host.h"
+/*#include "bcm_host.h"
 #include "interface/mmal/mmal.h"
 #include "interface/mmal/util/mmal_default_components.h"
 #include "interface/vcos/vcos.h"
 
 #include "interface/mmal/util/mmal_graph.h"
-#include "interface/mmal/util/mmal_util_params.h"
+#include "interface/mmal/util/mmal_util_params.h"*/
 
 #include <cstdint>
 
