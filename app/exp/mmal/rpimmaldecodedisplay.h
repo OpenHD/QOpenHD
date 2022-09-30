1b664e71cef19e5e7beda1a96246116759910d79
consti10 <geierconstantinabc@gmail.com>
2022-09-30 18:00:38 +0200

    exp

--- app/exp/mmal/rpimmaldecodedisplay.h
+++ app/exp/mmal/rpimmaldecodedisplay.h
@@ -6,7 +6,7 @@
 #include "interface/mmal/util/mmal_default_components.h"
 #include "interface/vcos/vcos.h"
 
-#include "interface/mmal/util/mmal_connection.h"
+#include "/opt/vc/include/interface/mmal/util/mmal_connection.h"
 
 #include "interface/mmal/util/mmal_graph.h"
 //#include "interface/mmal/util/mmal_util_params.h"
