b6ec67eaf35501303a085de99213e803146b562c
consti10 <geierconstantinabc@gmail.com>
2022-09-30 19:17:33 +0200

    exp

--- app/exp/avcodec_decoder.cpp
+++ app/exp/avcodec_decoder.cpp
@@ -756,7 +756,7 @@ void AVCodecDecoder::open_and_decode_until_error_custom_rtp(const QOpenHDVideoHe
                   keyframe_buf=m_rtp_receiver->get_config_data();
               }
               qDebug()<<"Got decode data (before keyframe)";
-#ifdef HAVE_MMAL
+#ifdef HAVE_MMALX
               RPIMMALDecoder::instance().initialize(keyframe_buf->data(),keyframe_buf->size(),640,480,30);
               has_keyframe_data=true;
 #else
@@ -777,7 +777,7 @@ void AVCodecDecoder::open_and_decode_until_error_custom_rtp(const QOpenHDVideoHe
                  }
                  buf=m_rtp_receiver->get_data();
              }
-#ifdef HAVE_MMAL
+#ifdef HAVE_MMALX
               RPIMMALDecoder::instance().feed_frame(buf->getData(),buf->getSize());
 #else
              //qDebug()<<"Got decode data (after keyframe)";
