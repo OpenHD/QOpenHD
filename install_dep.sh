9fc9cdecb2e92eaff6d39b311c511a68ec0bdb51
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-03 01:06:32 +0200

    repair graph

--- install_dep.sh
+++ install_dep.sh
@@ -3,10 +3,11 @@
 # Install all the dependencies needed to build QOpenHD from source
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
 apt -y install ruby ruby-dev libavcodec-dev libavformat-dev
-sudo apt-mark unhold libraspberrypi-dev
-apt -y install libraspberrypi-dev
 apt -y install mavsdk
 apt -y install git meson fmt
+
+# Repairing MMAL-Graph-Lib
+sed -i 's/util\/\mmal_connection.h/mmal_connection.h/g' /usr/include/interface/mmal/util/mmal_graph.h
 
 gem install fpm
 
