af636cbf36e3170a31fdd9aba9576fdf8c8d0dec
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-10-03 00:08:45 +0200

    unhold lib

--- install_dep.sh
+++ install_dep.sh
@@ -2,8 +2,9 @@
 
 # Install all the dependencies needed to build QOpenHD from source
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
-apt -y install ruby ruby-dev libraspberrypi-dev libavcodec-dev libavformat-dev
-apt -y install rubygems libgemplugin-ruby
+apt -y install ruby ruby-dev libavcodec-dev libavformat-dev
+sudo apt-mark unhold libraspberrypi-dev
+apt -y install libraspberrypi-dev
 apt -y install mavsdk
 apt -y install git meson fmt
 
