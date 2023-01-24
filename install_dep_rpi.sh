216796179bc32e1555893cc99b274101e2eb0333
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-24 21:33:32 +0100

    fix mavsdk

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -15,11 +15,15 @@ apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
 apt -y install ruby ruby-dev libavcodec-dev libavformat-dev
 apt -y install git meson
+apt -y install build-essential cmake git ruby-dev python3-pip || exit 1
+apt -y install cmake curl libjsoncpp-dev libtinyxml2-dev zlib1g libcurl4-gnutls-dev
+
+gem install fpm
+pip3 install --upgrade cloudsmith-cli
+pip3 install future
 
 # Repairing MMAL-Graph-Lib
 sed -i 's/util\/\mmal_connection.h/mmal_connection.h/g' /usr/include/interface/mmal/util/mmal_graph.h
-
-gem install fpm
 
 cd lib/MAVSDK
 cmake -Bbuild/default -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -H.
