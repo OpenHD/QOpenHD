0e59d2e93f888e8c58787d526117ca514af85942
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-24 20:44:17 +0100

    add jetson and rpi

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -12,11 +12,26 @@ apt install -y openhd-qt
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
 apt -y install ruby ruby-dev libavcodec-dev libavformat-dev
-apt -y install mavsdk
 apt -y install git meson
 
 # Repairing MMAL-Graph-Lib
 sed -i 's/util\/\mmal_connection.h/mmal_connection.h/g' /usr/include/interface/mmal/util/mmal_graph.h
 
 gem install fpm
+
+cd lib/MAVSDK
+cmake -Bbuild/default -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -H.
+cmake --build build/default -j4
+sudo cmake --build build/default --target install
+#sudo ldconfig
+
+ls /usr/local/include/mavsdk
+ls /usr/local/lib
+
+cp -r /usr/local/include/mavsdk /home/runner/work/QOpenHD/QOpenHD/mavsdk
+mkdir /home/runner/work/QOpenHD/QOpenHD/mavsdk/lib/
+cp /usr/local/lib/libmavsdk.a /home/runner/work/QOpenHD/QOpenHD/mavsdk/lib/
+ls /home/runner/work/QOpenHD/QOpenHD/mavsdk
+cd ../../
+
 
