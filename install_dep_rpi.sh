0ce4bd2bee9db4566678bb26b38c87e7847d8632
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-26 03:05:30 +0100

    I dont understand

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -13,11 +13,10 @@ apt -y install build-essential cmake git ruby-dev python3-pip || exit 1
 apt -y install cmake curl libjsoncpp-dev libtinyxml2-dev zlib1g libcurl4-gnutls-dev
 
 gem install fpm
-pip3 install --upgrade cloudsmith-cli
 pip3 install future
 
 # Repairing MMAL-Graph-Lib
-#sed -i 's/util\/\mmal_connection.h/mmal_connection.h/g' /usr/include/interface/mmal/util/mmal_graph.h
+sed -i 's/util\/\mmal_connection.h/mmal_connection.h/g' /usr/include/interface/mmal/util/mmal_graph.h
 
 # cd lib/MAVSDK
 # cmake -Bbuild/default -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -H.
