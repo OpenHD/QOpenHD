63861b31113874754a8c3a097a93939ac861177d
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-26 02:01:10 +0100

    debug

--- install_dep_rpi.sh
+++ install_dep_rpi.sh
@@ -4,12 +4,6 @@
 #initial requiremends
 apt install -y gnupg gnupg1 gnupg2 apt-transport-https curl apt-utils  libgles2-mesa-dev libegl1-mesa-dev libgbm-dev libboost-dev libsdl2-dev libsdl1.2-dev
 
-#install qt
-apt install -y openhd-qt
-ldconfig
-
-
-
 # Install all the dependencies needed to build QOpenHD from source
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
@@ -25,10 +19,10 @@ pip3 install future
 # Repairing MMAL-Graph-Lib
 sed -i 's/util\/\mmal_connection.h/mmal_connection.h/g' /usr/include/interface/mmal/util/mmal_graph.h
 
-cd lib/MAVSDK
-cmake -Bbuild/default -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -H.
-cmake --build build/default -j4
-sudo cmake --build build/default --target install
-cd ../../
+# cd lib/MAVSDK
+# cmake -Bbuild/default -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -H.
+# cmake --build build/default -j4
+# sudo cmake --build build/default --target install
+# cd ../../
 
 
