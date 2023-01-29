e98ab14ba591db2d1b740fc1cd655ff3c30cf088
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-29 22:05:59 +0100

    Update install_dep_ubuntu22.sh

--- install_dep_ubuntu22.sh
+++ install_dep_ubuntu22.sh
@@ -21,9 +21,8 @@ touch /etc/ld.so.conf.d/qt.conf
 echo "/opt/Qt5.15.7/lib/" >/etc/ld.so.conf.d/qt.conf
 sudo ldconfig
 export PATH="$PATH:/opt/Qt5.15.7/bin/"
-cd /usr/bin
-rm -f qmake
-sudo ln -s /opt/Qt5.15.7/bin/qmake qmake
+rm -f /usr/bin/qmake
+sudo ln -s /opt/Qt5.15.7/bin/qmake /usr/bin/qmake
 
 apt -y install libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good qtgstreamer-plugins-qt5
 
