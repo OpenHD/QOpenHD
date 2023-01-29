e69f09b00296be48941a1b707991cbdf90730240
consti10 <geierconstantinabc@gmail.com>
2023-01-29 19:29:06 +0100

    add script to build mavsdk

---
+++ lib/build_install_mavsdk_static.sh
@@ -0,0 +1,8 @@
+#bin/bash
+
+cd MAVSDK
+cmake -Bbuild/default -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -H.
+cmake --build build/default -j4
+sudo cmake --build build/default --target install
+
+# Now the static libs are installed and can be found and statically linked by qmake
