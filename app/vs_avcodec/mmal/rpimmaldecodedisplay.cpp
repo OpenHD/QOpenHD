7f9445d2d3143b4618d880fbbcb195fa7141462a
consti10 <geierconstantinabc@gmail.com>
2023-02-10 05:46:12 +0100

    disable it and check

--- app/vs_avcodec/mmal/rpimmaldecodedisplay.cpp
+++ app/vs_avcodec/mmal/rpimmaldecodedisplay.cpp
@@ -118,7 +118,7 @@ bool RPIMMalDecodeDisplay::initialize(const uint8_t *config_data, const int conf
     // Do it anyways
     // Weird - this flag seems to eliminte the "720p 60fps issue" even thugh I have no idea why
     // If the data is known to be framed then the following flag should be set:
-    format_in->flags |= MMAL_ES_FORMAT_FLAG_FRAMED;
+    //format_in->flags |= MMAL_ES_FORMAT_FLAG_FRAMED;
 
     //SOURCE_READ_CODEC_CONFIG_DATA(codec_header_bytes, codec_header_bytes_size);
     m_status = mmal_format_extradata_alloc(format_in, config_data_size);
