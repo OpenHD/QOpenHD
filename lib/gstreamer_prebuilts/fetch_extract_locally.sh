0c18cb6a5862a3b712a2ac88a5d81e0340810188
consti10 <geierconstantinabc@gmail.com>
2023-03-14 22:05:24 +0100

    debug CIs

--- lib/gstreamer_prebuilts/fetch_extract_locally.sh
+++ lib/gstreamer_prebuilts/fetch_extract_locally.sh
@@ -1,8 +1,10 @@
 #bin/bash
 
-wget https://gstreamer.freedesktop.org/data/pkg/android/1.20.5/gstreamer-1.0-android-universal-1.20.5.tar.xz
+wget --quiet https://gstreamer.freedesktop.org/data/pkg/android/1.20.5/gstreamer-1.0-android-universal-1.20.5.tar.xz
 
 mkdir -p gstreamer-1.0-android-universal-1.20.5
 
 tar xf gstreamer-1.0-android-universal-1.20.5.tar.xz -C gstreamer-1.0-android-universal-1.20.5
+
+ls
 
