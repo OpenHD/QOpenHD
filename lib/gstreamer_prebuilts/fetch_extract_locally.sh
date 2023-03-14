0610e04011826a6930b819aa864dc1dbb3096d83
consti10 <geierconstantinabc@gmail.com>
2023-03-14 21:31:44 +0100

    try fix android CI

--- lib/gstreamer_prebuilts/fetch_extract_locally.sh
+++ lib/gstreamer_prebuilts/fetch_extract_locally.sh
@@ -2,5 +2,5 @@
 
 wget https://gstreamer.freedesktop.org/data/pkg/android/1.20.5/gstreamer-1.0-android-universal-1.20.5.tar.xz
 
-tar xf gstreamer-1.0-android-universal-1.20.5.tar.xz
+tar xf gstreamer-1.0-android-universal-1.20.5.tar.xz -C gstreamer-1.0-android-universal-1.20.5
 
