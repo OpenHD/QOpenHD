ff3a6f48acf6e17bad3affbc6db7a70ad9f69eaf
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-26 15:28:41 +0200

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -34,23 +34,23 @@ bash build_install_mavsdk_static.sh || exit 1
 # Main function
  
  if [[ "${PLATFORM}" == "rpi" ]]; then
-    echo "rpi"
+    install_pi_packages
     # Repairing MMAL-Graph-Lib
     sed -i 's/util\/\mmal_connection.h/mmal_connection.h/g' /usr/include/interface/mmal/util/mmal_graph.h
  elif [[ "${PLATFORM}" == "jetson" ]] ; then
-    echo "jetson"
+    install_jetson_packages
  elif [[ "${PLATFORM}" == "ubuntu-x86" ]] ; then
-    echo "ubuntu-x86"
+    install_x86_packages
  elif [[ "${PLATFORM}" == "rock5" ]] ; then
-    echo "rock5"
+    install_rock_packages
  else
     echo "platform not supported"
  fi
 
  if [[ "${QTTYPE}" == "custom" ]]; then
-    echo "custom"
+    apt install -y openhd-qt 
  else
-    echo "stock"
+    apt -y install qml-module-qtquick-controls2 libqt5concurrent5 libqt5core5a libqt5dbus5 libqt5designer5 libqt5gui5 libqt5help5 libqt5location5 libqt5location5-plugins libqt5multimedia5 libqt5multimedia5-plugins libqt5multimediagsttools5 libqt5multimediawidgets5 libqt5network5 libqt5opengl5 libqt5opengl5-dev libqt5positioning5 libqt5positioning5-plugins libqt5positioningquick5 libqt5printsupport5 libqt5qml5 libqt5quick5 libqt5quickparticles5 libqt5quickshapes5 libqt5quicktest5 libqt5quickwidgets5 libqt5sensors5 libqt5sql5 libqt5sql5-sqlite libqt5svg5 libqt5test5 libqt5webchannel5 libqt5webkit5 libqt5widgets5 libqt5x11extras5 libqt5xml5 openshot-qt python3-pyqt5 python3-pyqt5.qtopengl python3-pyqt5.qtsvg python3-pyqt5.qtwebkit python3-pyqtgraph qml-module-qt-labs-settings qml-module-qtgraphicaleffects qml-module-qtlocation qml-module-qtpositioning qml-module-qtquick-controls qml-module-qtquick-dialogs qml-module-qtquick-extras qml-module-qtquick-layouts qml-module-qtquick-privatewidgets qml-module-qtquick-shapes qml-module-qtquick-window2 qml-module-qtquick2 qt5-gtk-platformtheme qt5-qmake qt5-qmake-bin qt5-qmltooling-plugins qtbase5-dev qtbase5-dev-tools qtchooser qtdeclarative5-dev qtdeclarative5-dev-tools qtpositioning5-dev qttranslations5-l10n
  fi
 
  # Install platform-specific packages
