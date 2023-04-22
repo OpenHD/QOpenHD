1e6fd6a59e3f06a6739ded977c30a1248af46ef4
ru26kif <constantin.geier@campus.lmu.de>
2023-04-22 11:39:37 +0200

    wtf is that

--- lib/h264/zero_memory.cc
+++ lib/h264/zero_memory.cc
@@ -30,7 +30,7 @@ void ExplicitZeroMemory(void* ptr, size_t len) {
   /* As best as we can tell, this is sufficient to break any optimisations that
      might try to eliminate "superfluous" memsets. If there's an easy way to
      detect memset_s, it would be better to use that. */
-  __asm__ __volatile__("" : : "r"(ptr) : "memory");  // NOLINT
+  //__asm__ __volatile__("" : : "r"(ptr) : "memory");  // NOLINT
 #endif
 #endif  // !WEBRTC_WIN
 }
