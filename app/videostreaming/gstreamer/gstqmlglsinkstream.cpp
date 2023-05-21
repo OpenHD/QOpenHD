73447abe2d10fe0617eec3ed22a0a472fa2bc321
consti10 <geierconstantinabc@gmail.com>
2023-05-21 21:58:21 +0200

    more documentation

--- app/videostreaming/gstreamer/gstqmlglsinkstream.cpp
+++ app/videostreaming/gstreamer/gstqmlglsinkstream.cpp
@@ -76,6 +76,7 @@ static std::string gst_create_video_decoder(const QOpenHDVideoHelper::VideoCodec
         ss<<"h264parse ! ";
         ss<<"decodebin ! ";
     }else if(videoCodec==QOpenHDVideoHelper::VideoCodecH265){
+        //ss<<"h265parse ! ";
         ss<<"decodebin ! ";
     }else{
        ss<<"decodebin ! ";
