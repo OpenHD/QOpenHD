70f6af6233c290ef07d97ffc4b3dea20d70d39b7
consti10 <geierconstantinabc@gmail.com>
2023-05-29 12:45:35 +0200

    do not clone the test videos on openhd images (they only take up valuable space)

--- after-install.sh
+++ after-install.sh
@@ -3,10 +3,10 @@
 # the openhd scripts start the correct OSD depending on the distro and settings
 systemctl enable qopenhd.service
 
-cd /usr/local/share/
-if [ -d "/usr/local/share/testvideos" ]; then
-  # Take action if $DIR exists. #
-  echo "Testvideos are already there"
-else
-git clone https://github.com/OpenHD/testvideos/
-fi
+#cd /usr/local/share/
+#if [ -d "/usr/local/share/testvideos" ]; then
+#  # Take action if $DIR exists. #
+#  echo "Testvideos are already there"
+#else
+#git clone https://github.com/OpenHD/testvideos/
+#fi
