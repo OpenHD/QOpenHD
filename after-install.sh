ecb97e2bfe9b885751444c32d5cf6c1293f03648
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-06-03 00:06:24 +0200

    add exeption for x86

--- after-install.sh
+++ after-install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # the openhd scripts start the correct OSD depending on the distro and settings
-systemctl enable qopenhd.service
-
+if [ -f /etc/systemd/system/qopenhd.service ]; then
+    systemctl enable qopenhd.service
+else
+    echo "QOpenHD is not started with a service on this system"
+fi
 #cd /usr/local/share/
 #if [ -d "/usr/local/share/testvideos" ]; then
 #  # Take action if $DIR exists. #
