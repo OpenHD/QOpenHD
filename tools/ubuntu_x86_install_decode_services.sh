117c1dad508a5dc23f6b6ee7653968430382afdd
consti10 <geierconstantinabc@gmail.com>
2023-07-24 12:37:23 +0200

    external decode service(s) for ubuntu

--- tools/ubuntu_x86_install_decode_services.sh
+++ tools/ubuntu_x86_install_decode_services.sh
@@ -1,5 +1,14 @@
 #bin/bash
 
+# stop and remove previous
+for value in h264_decode h265_decode mjpeg_decode
+do
+  echo $value
+  sudo systemctl stop $value
+  sudo systemctl disable $value
+  sudo rm -rf /etc/systemd/system/$value.service
+done
+
 sudo cp ../systemd/x86_ubuntu_h264_decode.service /etc/systemd/system/h264_decode.service
 
 sudo cp ../systemd/x86_ubuntu_h265_decode.service /etc/systemd/system/h265_decode.service
@@ -18,3 +27,4 @@ sudo systemctl stop mjpeg_decode
 #sudo systemctl disable mjpeg_decode
 
 
+sudo systemctl daemon-reload
