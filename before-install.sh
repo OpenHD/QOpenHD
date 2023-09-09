d4155ecf85ac0faee47e03f5ce69f685fe73add6
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-09-09 20:35:06 +0200

    remove custom qt and increase version

---
+++ before-install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#remove custom QT
+sudo apt remove -y openhd-qt-jammy
--- package.sh
+++ package.sh
@@ -85,11 +85,20 @@ elif [[ "${PACKAGE_ARCH}" = "arm64" ]]; then
 cp rock_qt_eglfs_kms_config.json /tmp/qopenhd/usr/local/share/qopenhd/ || exit 1
 fi
 
-VERSION="2.3-evo-$(date '+%Y%m%d%H%M')-${VER2}"
+VERSION="2.5-evo-$(date '+%Y%m%d%H%M')-${VER2}"
 
 rm ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
 ls -a
+
+if [[ "${DISTRO}" == "jammy" ]] && [[ "${OS}" == "ubuntu" ]] ; then
+fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION} -C ${TMPDIR} \
+  -p qopenhd_VERSION_ARCH.deb \
+  --after-install after-install.sh \
+  --before-install before-install.sh \
+  ${PLATFORM_PACKAGES} || exit 1
+else
 fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION} -C ${TMPDIR} \
   -p qopenhd_VERSION_ARCH.deb \
   --after-install after-install.sh \
   ${PLATFORM_PACKAGES} || exit 1
+fi
