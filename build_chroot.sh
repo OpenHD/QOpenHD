4a57c8e44753a9e5eb816d50ccf139bf9e4b9e06
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-09-27 20:07:25 +0200

    add release branch

--- build_chroot.sh
+++ build_chroot.sh
@@ -11,8 +11,8 @@ cp -v *.dep /opt/out/
 echo "copied deb file"
 echo "push to cloudsmith"
 git describe --exact-match HEAD >/dev/null 2>&1
-echo "Pushing the package to OpenHD 2.4 repository"
+echo "Pushing the package to OpenHD release repository"
 ls -a
 API_KEY=$(cat cloudsmith_api_key.txt)
-cloudsmith push deb --api-key "$API_KEY" openhd/openhd-2-3-evo/debian/bullseye *.deb || exit 1
+cloudsmith push deb --api-key "$API_KEY" openhd/release/debian/bullseye *.deb || exit 1
 
