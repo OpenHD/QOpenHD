43341515f46e5af36e340349f056d6950ba2f74a
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-21 22:34:22 +0100

    fix x86 packaging

--- package.sh
+++ package.sh
@@ -87,7 +87,7 @@ fi
 VERSION="2.5.3-$(date '+%Y%m%d%H%M')-${VER2}"
 
 rm ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
-if [[ "${PACKAGE_ARCH}" = "armhf" ]]; then
+if [[ "${PACKAGE_ARCH}" = "armhf" ]] || [[ "${PACKAGE_ARCH}" = "x86_64" ]]; then
     fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION} -C ${TMPDIR} \
     -p qopenhd_VERSION_ARCH.deb \
     --after-install after-install.sh \
