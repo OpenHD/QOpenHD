7eb4e06e36a23697b48a90c4041f471bf50b6380
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-23 14:31:13 +0100

    Dev release (#618)
    * create dev release
    
    * fix x86 packaging
    
    * Update package.sh
    
    * Update package.sh
    
    * Update package.sh
    
    * Update build_package_x86_lunar.yml
    
    * Update AppSettings.qml

--- package.sh
+++ package.sh
@@ -17,9 +17,6 @@ rm -rf /tmp/qopenhd/*
 mkdir -p /tmp/qopenhd/usr/local/bin || exit 1
 mkdir -p /tmp/qopenhd/etc/systemd/system || exit 1
 
-ls -a
-ls /opt
-
 VER2=$(git rev-parse --short HEAD)
 
 
@@ -56,6 +53,8 @@ PLATFORM_PACKAGES="-d openhd-userland -d libavcodec-dev -d libavformat-dev -d op
 else
 PLATFORM_PACKAGES="-d qml-module-qtcharts -d gstreamer1.0-gl -d qtgstreamer-plugins-qt5 -d gstreamer1.0-qt5 -d libqt5texttospeech5-dev -d qml-module-qt-labs-platform -d git -d libgstreamer-plugins-base1.0-dev -d gstreamer1.0-plugins-good -d libavcodec-dev -d libavformat-dev -d qml-module-qtquick-controls2 -d libqt5concurrent5 -d libqt5core5a -d libqt5dbus5 -d libqt5gui5 -d libqt5help5 -d libqt5location5 -d libqt5location5-plugins -d libqt5multimedia5 -d libqt5multimedia5-plugins -d libqt5multimediagsttools5 -d libqt5multimediawidgets5 -d libqt5network5 -d libqt5opengl5 -d libqt5opengl5-dev -d libqt5positioning5 -d libqt5positioning5-plugins -d libqt5positioningquick5 -d libqt5printsupport5 -d libqt5qml5 -d libqt5quick5 -d libqt5quickparticles5 -d libqt5quickshapes5 -d libqt5quicktest5 -d libqt5quickwidgets5 -d libqt5sensors5 -d libqt5sql5 -d libqt5sql5-sqlite -d libqt5svg5 -d libqt5test5 -d libqt5webchannel5 -d libqt5webkit5 -d libqt5widgets5 -d libqt5x11extras5 -d libqt5xml5 -d openshot-qt -d python3-pyqt5 -d python3-pyqt5.qtopengl -d python3-pyqt5.qtsvg -d python3-pyqt5.qtwebkit -d python3-pyqtgraph -d qml-module-qt-labs-settings -d qml-module-qtgraphicaleffects -d qml-module-qtlocation -d qml-module-qtpositioning -d qml-module-qtquick-controls -d qml-module-qtquick-dialogs -d qml-module-qtquick-extras -d qml-module-qtquick-layouts -d qml-module-qtquick-privatewidgets -d qml-module-qtquick-shapes -d qml-module-qtquick-window2 -d qml-module-qtquick2 -d qt5-gtk-platformtheme -d qt5-qmake -d qt5-qmake-bin -d qt5-qmltooling-plugins -d qtbase5-dev -d qtbase5-dev-tools -d qtchooser -d qtdeclarative5-dev -d qtdeclarative5-dev-tools -d qtpositioning5-dev -d qttranslations5-l10n"
 fi
+ls -a release/
+echo "here we should have built the file correctly"
 
 cp release/QOpenHD /tmp/qopenhd/usr/local/bin/ || exit 1
 
@@ -84,15 +83,20 @@ elif [[ "${PACKAGE_ARCH}" = "arm64" ]]; then
 cp rock_qt_eglfs_kms_config.json /tmp/qopenhd/usr/local/share/qopenhd/ || exit 1
 fi
 
-VERSION="2.5.3-$(date '+%Y%m%d%H%M')-${VER2}"
+VERSION="2.5.2-beta-$(date '+%Y%m%d%H%M')-${VER2}"
+
+ls -a ${TMPDIR}/usr/local/bin
+echo "going into packaging stage"
 
 rm ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
 if [[ "${PACKAGE_ARCH}" = "armhf" ]]; then
+    echo "raspberry pi"
     fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION} -C ${TMPDIR} \
     -p qopenhd_VERSION_ARCH.deb \
     --after-install after-install.sh \
     ${PLATFORM_PACKAGES} || exit 1
 elif [[ "${PACKAGE_ARCH}" = "arm64" ]]; then
+    echo "rockchip"
     fpm -a ${PACKAGE_ARCH} -s dir -t deb -n qopenhd_rk3566 -v ${VERSION} -C ${TMPDIR} \
     -p qopenhd_rk3566_VERSION_ARCH.deb \
     --after-install after-install.sh \
@@ -109,6 +113,13 @@ elif [[ "${PACKAGE_ARCH}" = "arm64" ]]; then
 
     fpm -a ${PACKAGE_ARCH} -s dir -t deb -n qopenhd_rk3588 -v ${VERSION} -C ${TMPDIR} \
     -p qopenhd_rk3588_VERSION_ARCH.deb \
+    --after-install after-install.sh \
+    ${PLATFORM_PACKAGES} || exit 1
+else
+    echo "everything else"
+
+    fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION} -C ${TMPDIR} \
+    -p qopenhd_VERSION_ARCH.deb \
     --after-install after-install.sh \
     ${PLATFORM_PACKAGES} || exit 1
 fi
