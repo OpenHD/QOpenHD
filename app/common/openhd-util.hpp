cf041df824d4e736b801097af06c7455d1799ef1
Oleg <olegnowmenco@gmail.com>
2024-01-29 22:38:25 +0200

    remove unclear define

--- app/common/openhd-util.hpp
+++ app/common/openhd-util.hpp
@@ -10,7 +10,6 @@
 #include <memory>
 
 #if defined(__linux__) || defined(__macos__)
-#define UNIX_STYLE_OS
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #endif // __linux__ || __macos__
@@ -28,7 +27,7 @@ namespace OHDUtil {
  * NOTE: Used to use boost, there were issues with that, I changed it to use c standard library.
  */
 static bool run_command(const std::string &command, const std::vector<std::string> &args,bool print_debug=true) {
-#ifdef UNIX_STYLE_OS
+#if defined(__linux__) || defined(__macos__)
   std::stringstream ss;
   ss << command;
   for (const auto &arg: args) {
@@ -68,7 +67,7 @@ static bool run_command(const std::string &command, const std::vector<std::strin
  */
 
 static std::optional<std::string> run_command_out(const char* command){
-#ifdef UNIX_STYLE_OS
+#if defined(__linux__) || defined(__macos__)
   std::string raw_value;
   std::array<char, 512> buffer{};
   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command, "r"), pclose);
@@ -94,7 +93,7 @@ static std::string yes_or_no(bool yes){
 }
 
 static bool is_valid_ip(const std::string& ip) {
-#ifdef UNIX_STYLE_OS
+#if defined(__linux__) || defined(__macos__)
   unsigned char buf[sizeof(struct in6_addr)];
   auto result = inet_pton(AF_INET, ip.c_str(), buf);
   return result == 1;
