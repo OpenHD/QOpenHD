dd0b586bdffa12db6858d147e63edbd8568efb66
consti10 <geierconstantinabc@gmail.com>
2024-02-28 03:38:23 +0100

    update gst version

--- lib/gstreamer_prebuilts/fetch_extract_locally.sh
+++ lib/gstreamer_prebuilts/fetch_extract_locally.sh
@@ -1,6 +1,6 @@
 #bin/bash
 
-wget --quiet https://gstreamer.freedesktop.org/data/pkg/android/1.20.5/gstreamer-1.0-android-universal-1.22.10.tar.xz
+wget --quiet https://gstreamer.freedesktop.org/data/pkg/android/1.22.10/gstreamer-1.0-android-universal-1.22.10.tar.xz
 
 # NOTE: We intentionally do not include the version in the extracted folder
 mkdir -p gstreamer-1.0-android-universal
