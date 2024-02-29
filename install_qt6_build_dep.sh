913e90f23da0701f2094b9d7c4fcda07ef0425ba
consti10 <geierconstantinabc@gmail.com>
2024-03-01 00:36:21 +0100

    add QT6 CI

---
+++ install_qt6_build_dep.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+apt-get install -y build-essential cmake
+apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
+apt-get install -y libavcodec-dev libavformat-dev
