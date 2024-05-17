691036e3d84b418384670ece239a79ffba6d54f2
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-17 02:20:21 +0200

    use new chroot compiler

--- build_chroot.sh
+++ build_chroot.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-#This file is the install instruction for the CHROOT build
-#We're using cloudsmith-cli to upload the file in CHROOT
+# This file is the install instruction for the CHROOT build
+# We're using cloudsmith-cli to upload the file in CHROOT
 
-echo "____________build like a rockSTAR___________"
 sudo apt install -y python3-pip git
-sudo pip3 install --upgrade cloudsmith-cli
-bash install_build_dep.sh rock5
-mkdir -p /usr/local/share/openhd/platform/rock/
-sudo ./package.sh arm64 debian bullseye-rock5 || exit 1
-mkdir -p /opt/out/
-cp -v *.dep /opt/out/
-echo "copied deb file"
-echo "push to cloudsmith"
-git describe --exact-match HEAD >/dev/null 2>&1
-echo "Pushing the package to OpenHD dev-release repository"
-ls -a
-API_KEY=$(cat cloudsmith_api_key.txt)
-cloudsmith push deb --api-key "$API_KEY" openhd/release/debian/bullseye qopenhd_rk3566*.deb || exit 1
-cloudsmith push deb --api-key "$API_KEY" openhd/release/debian/bullseye qopenhd_rk3588a*.deb || exit 1
-rm -Rf qopenhd_rk3588a*.deb
-cloudsmith push deb --api-key "$API_KEY" openhd/release/debian/bullseye qopenhd_rk3588*.deb || exit 1
+DISTRO=$(cat distro.txt)
+FLAVOR=$(cat flavor.txt)
+REPO=$(cat repo.txt)
+CUSTOM=$(cat custom.txt)
+ARCH=$(cat arch.txt)
 
+echo ${DISTRO}
+echo ${FLAVOR}
+echo ${CUSTOM}
+echo ${ARCH}
+
+bash install_build_dep.sh rock5
+sudo ./package.sh ${CUSTOM} ${ARCH} ${DISTRO} ${FLAVOR} || exit 1
 
