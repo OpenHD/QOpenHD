e79a63eb8cdd0b4a8e0fedd5434607c19c13d6be
Aleksey Degtyarev <degterev_alex@mail.ru>
2024-10-18 16:10:06 +0700

    Update documentedparam.cpp

--- app/telemetry/settings/documentedparam.cpp
+++ app/telemetry/settings/documentedparam.cpp
@@ -98,7 +98,7 @@ static std::vector<std::shared_ptr<XParam>> get_parameters_list(){
         //58: 420-450 mW
         // NOTE: We now have the tx power wizzard !
         auto values_WB_TX_PWR_INDEX=std::vector<ImprovedIntSetting::Item>{
-            {"Unitless [1]",3},
+            {"Unitless [1]",1},
             {"Unitless [3]",3},
             {"Unitless [6]",6},
             {"Unitless [10]",10},
@@ -109,7 +109,7 @@ static std::vector<std::shared_ptr<XParam>> get_parameters_list(){
             {"Unitless [63]",63},                                                                   };
         auto values_WB_TX_PWR_INDEX_ARMED=std::vector<ImprovedIntSetting::Item>{
             {"Disabled [0]",0},
-            {"Unitless [1]",3},
+            {"Unitless [1]",1},
             {"Unitless [3]",3},
             {"Unitless [6]",6},
             {"Unitless [10]",10},
