60cf970d8658ef0fde1706885fa19f472b33d1d2
Raphael <raphael@openhdfpv.org>
2025-07-12 01:07:23 +0200

    testing

--- lib/gstreamer_prebuilts/fetch_extract_locally.sh
+++ lib/gstreamer_prebuilts/fetch_extract_locally.sh
@@ -1,11 +1,11 @@
-#bin/bash
+# #bin/bash
 
-wget --quiet https://gstreamer.freedesktop.org/data/pkg/android/1.20.7/gstreamer-1.0-android-universal-1.20.7.tar.xz
+# wget --quiet https://gstreamer.freedesktop.org/data/pkg/android/1.20.7/gstreamer-1.0-android-universal-1.20.7.tar.xz
 
-# NOTE: We intentionally do not include the version in the extracted folder
-mkdir -p gstreamer-1.0-android-universal
+# # NOTE: We intentionally do not include the version in the extracted folder
+# mkdir -p gstreamer-1.0-android-universal
 
-tar xf gstreamer-1.0-android-universal-* -C gstreamer-1.0-android-universal
+# tar xf gstreamer-1.0-android-universal-* -C gstreamer-1.0-android-universal
 
-ls
+# ls
 
