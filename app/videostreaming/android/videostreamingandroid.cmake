60c975fff9b5cfff9569636eb148114324e9da69
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-08-16 03:33:46 +0200

    Update videostreamingandroid.cmake

--- app/videostreaming/android/videostreamingandroid.cmake
+++ app/videostreaming/android/videostreamingandroid.cmake
@@ -40,7 +40,7 @@ set_target_properties(lowlag_android PROPERTIES
 target_include_directories(lowlag_android
     PUBLIC
         ${CMAKE_CURRENT_SOURCE_DIR}  
-        ${CMAKE_CURRENT_SOURCE_DIR}/aarch64 
+        ${CMAKE_CURRENT_SOURCE_DIR}/aarch64/include/gstreamer-1.0/
         ${CMAKE_CURRENT_SOURCE_DIR}/lib/h264
         ${CMAKE_CURRENT_SOURCE_DIR}/app/videostreaming
         ${CMAKE_CURRENT_SOURCE_DIR}/app/videostreaming/vscommon
