e5875ad793b18c92835a21e6f63273fe9f5717fb
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-08-16 14:16:36 +0200

    Update qsurfacetexture.cpp

--- app/videostreaming/android/qsurfacetexture.cpp
+++ app/videostreaming/android/qsurfacetexture.cpp
@@ -33,7 +33,13 @@
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 // Needed for QNativeInterface::QSGOpenGLTexture::fromNativeExternalOES
-#include <qsgtextureplatform.h>
+#  if __has_include(<QtQuick/qsgtextureplatform.h>)
+#    include <QtQuick/qsgtextureplatform.h>
+#  elif __has_include(<qsgtextureplatform.h>)
+#    include <qsgtextureplatform.h>
+#  else
+#    error "qsgtextureplatform.h not found. Ensure Qt Quick is installed and included."
+#  endif
 #endif
 
 #include "../vscommon/QOpenHDVideoHelper.hpp"
@@ -61,11 +67,9 @@ public:
         m_qsgTexture = QNativeInterface::QSGOpenGLTexture::fromNativeExternalOES(
                 m_textureId, m_window, initialSize, {});
 #else
-        // Fallback (should not happen on your Qt 6.9, but keeps the code compilable)
         m_qsgTexture = QNativeInterface::QSGOpenGLTexture::fromNative(
                 m_textureId, m_window, initialSize, {});
 #endif
-        // Material that samples the QSGTexture
         auto *mat = new QSGTextureMaterial();
         mat->setTexture(m_qsgTexture);
         setMaterial(mat);
@@ -85,26 +89,20 @@ public:
             env->DeleteGlobalRef(m_uSTMatrixArray);
         m_uSTMatrixArray = nullptr;
 
-        // QSGTexture is *not* owned by the material, so clean it up.
         delete m_qsgTexture;
         m_qsgTexture = nullptr;
     }
 
     void setRect(const QRectF &rect, const QRectF &texRect)
     {
-        // Basic rect + (0..1) tex coords. If you later want to apply uSTMatrix,
-        // you can compute transformed UVs here and fill vertex data manually.
         QSGGeometry::updateTexturedRectGeometry(&m_geometry, rect, texRect);
         markDirty(QSGNode::DirtyGeometry | QSGNode::DirtyMaterial);
     }
 
-    // QSGNode interface
     void preprocess() override
     {
-        // Update the texture content
         m_surfaceTexture.callMethod<void>("updateTexImage");
-
-        // If you need the transform, keep retrieving it here (render thread):
+        // If needed later:
         // m_surfaceTexture.callMethod<void>("getTransformMatrix", "([F)V", m_uSTMatrixArray);
         // QAndroidJniEnvironment env;
         // env->GetFloatArrayRegion(m_uSTMatrixArray, 0, 16, m_uSTMatrix.data());
@@ -129,7 +127,6 @@ QSurfaceTexture::QSurfaceTexture(QQuickItem *parent)
 
 QSurfaceTexture::~QSurfaceTexture()
 {
-    // Delete our texture
     if (m_textureId) {
         glBindTexture(GL_TEXTURE_EXTERNAL_OES, 0);
         glDeleteTextures(1, &m_textureId);
@@ -143,7 +140,6 @@ void QSurfaceTexture::set_video_texture_size(int width_px, int height_px)
     m_texture_height_px = height_px;
 }
 
-// Flip vertically. Despite the name, this swaps top/bottom to correct orientation on Android.
 static void qrectf_flip_horizontally(QRectF& rect){
     float tmp = rect.top();
     rect.setTop(rect.bottom());
@@ -154,22 +150,16 @@ QSGNode *QSurfaceTexture::updatePaintNode(QSGNode *n, QQuickItem::UpdatePaintNod
 {
     auto *node = static_cast<SurfaceTextureNode *>(n);
     if (!node) {
-        // Create the external OES texture
         glGenTextures(1, &m_textureId);
         glBindTexture(GL_TEXTURE_EXTERNAL_OES, m_textureId);
 
-        // Can't do mipmapping with camera/video source
         glTexParameterf(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameterf(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        // Clamp to edge is the only option
         glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_EXTERNAL_OES, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-        // Create the Java SurfaceTexture bound to our GL texture
         m_surfaceTexture = QAndroidJniObject("android/graphics/SurfaceTexture", "(I)V", m_textureId);
 
-        // Build the scene graph node around it
         const QSize texSize = (m_texture_width_px > 0 && m_texture_height_px > 0)
                                 ? QSize(m_texture_width_px, m_texture_height_px)
                                 : QSize(1, 1);
@@ -188,16 +178,11 @@ QSGNode *QSurfaceTexture::updatePaintNode(QSGNode *n, QQuickItem::UpdatePaintNod
         rect = QRectF(coords.x, coords.y, coords.width, coords.height);
     }
 
-    // Flip vertical - otherwise video can appear upside down on Android
     qrectf_flip_horizontally(rect);
 
-    // For now we use the basic 0..1 texture rect. If you need the Android uSTMatrix
-    // applied (crop/rotate), we can extend setRect() to write transformed UVs.
     const QRectF texture_coords = QRectF(0, 0, 1, 1);
-
     node->setRect(rect, texture_coords);
 
-    // Schedule a repaint on the GUI thread
     QMetaObject::invokeMethod(reinterpret_cast<QSurfaceTexture*>(this), "update", Qt::QueuedConnection);
     return node;
 }
