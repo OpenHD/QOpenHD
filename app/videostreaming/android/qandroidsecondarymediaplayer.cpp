91ba5ca93d9727587c30e64f146a587855f79cc8
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-11-03 01:22:23 +0100

    Update qandroidsecondarymediaplayer.cpp

--- app/videostreaming/android/qandroidsecondarymediaplayer.cpp
+++ app/videostreaming/android/qandroidsecondarymediaplayer.cpp
@@ -13,7 +13,7 @@ namespace {
 QString buildStreamUrl(const QOpenHDVideoHelper::VideoStreamConfigXX &config)
 {
     const QString ip = QString::fromStdString(config.udp_rtp_input_ip_address).trimmed();
-    const int port = config.udp_rtp_input_port;
+    const int port = config.udp_rtp_input_port_secondary;
 
     if (ip.isEmpty())
         return {};
