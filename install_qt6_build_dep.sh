c84b1bd4e4aea5b1e73007c510566137815c0b42
Roman Buldygin <froooks@gmail.com>
2026-01-18 00:35:35 +0300

    VS for development on linux

--- install_qt6_build_dep.sh
+++ install_qt6_build_dep.sh
@@ -3,3 +3,4 @@
 apt-get install -y build-essential cmake
 apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good
 apt-get install -y libavcodec-dev libavformat-dev
+apt-get install -y qt6-tools-dev qt6-declarative-dev qt6-declarative-dev-tools ninja-build qt6-base-dev
