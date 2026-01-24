152531da3a4bea496f5d52a3c44eba14120998dd
Roman Buldygin <froooks@gmail.com>
2026-01-24 22:44:38 +0300

    rename

---
+++ app/videostreaming/v4l2/EncodedDmaBuffersQueue.h
@@ -0,0 +1,20 @@
+#ifndef QOPENHDPROJECT_ENCODEDDMABUFFERSQUEUE_H
+#define QOPENHDPROJECT_ENCODEDDMABUFFERSQUEUE_H
+
+
+
+class EncodedDmaBuffersQueue {
+private:
+
+
+public:
+    EncodedDmaBuffersQueue(const EncodedDmaBuffersQueue&) = delete;
+    EncodedDmaBuffersQueue& operator=(const EncodedDmaBuffersQueue&) = delete;
+    EncodedDmaBuffersQueue(EncodedDmaBuffersQueue&&) = delete;
+    EncodedDmaBuffersQueue& operator=(EncodedDmaBuffersQueue&&) = delete;
+
+};
+
+
+
+#endif //QOPENHDPROJECT_ENCODEDDMABUFFERSQUEUE_H
